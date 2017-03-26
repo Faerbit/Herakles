92c7e407cc73e90242667d599452a2e99a3a8472
Faerbit <faerbit@gmail.com>
2017-03-26 23:32:52 +0200

    GPLed.

--- app/src/main/java/it/faerb/herakles/LocationLogListFragment.java
+++ app/src/main/java/it/faerb/herakles/LocationLogListFragment.java
@@ -1,3 +1,11 @@
+/*
+ * Herakles - Sports Activity Tracking App for Android
+ * Copyright (c) 2017 Fabian Klemp
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.faerb.herakles;
 
 import android.os.Bundle;
